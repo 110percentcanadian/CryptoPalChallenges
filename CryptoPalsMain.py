3720ece88b619fbd1dcc322028a46ed16a96e5fa
jeff <jeffdoyle17@gmail.com>
2020-03-28 21:02:58 -0700

    Hex to base 64 functioning woo!

--- CryptoPalsMain.py
+++ CryptoPalsMain.py
@@ -1,3 +1,19 @@
 
 # main program/script for the cyptopals challenges
-hexString = 0xf4762
+#might evolve into a few methods and main or something
+import codecs
+
+
+def hexString2Base64(wrkStr):
+    hexString = wrkStr
+    #now decode is in the codecs module, still built in
+    BaseNewStr = codecs.encode(hexString)
+    deCodeString = codecs.decode(BaseNewStr,'hex_codec')
+    Base64Str = codecs.encode(deCodeString,'base64_codec')
+    return Base64Str
+
+
+if __name__ == '__main__':
+    hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    Base64newby = hexString2Base64(wrkStr=hexString)
+    print(Base64newby)
